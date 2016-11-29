c7df175a7aca65335a32c05b96ade557ae146bb2
cesare90 <cesaredepadua@gmail.com>
2016-11-29 09:22:35 +0100

    initialiing app

---
+++ Umanis_planning_v.1.0/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name umanisPlanningV10App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the umanisPlanningV10App
+ */
+angular.module('umanisPlanningV10App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ Umanis_planning_v.1.0/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name umanisPlanningV10App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the umanisPlanningV10App
+ */
+angular.module('umanisPlanningV10App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
